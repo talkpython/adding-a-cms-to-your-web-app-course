e331b42b1af80bcd3ecb5c39ce75a3381dd85778
Michael Kennedy <mikeckennedy@gmail.com>
2020-05-30 18:52:46 -0700

    Starter code for chapter 7, saving content.

---
+++ code/flask/ch07-saving-content/pypi_flask_ch07/pypi_org/services/cms_service.py
@@ -0,0 +1,106 @@
+import random
+from typing import Optional, Dict, List
+
+from pypi_org.db import fake_data
+
+
+def get_page(base_url: str) -> Optional[Dict]:
+    if not base_url or not base_url.strip():
+        return None
+
+    base_url = base_url.strip().lower()
+
+    page = fake_data.pages.get(base_url)
+    return page
+
+
+def get_redirect(base_url: str) -> Optional[Dict]:
+    if not base_url or not base_url.strip():
+        return None
+
+    base_url = base_url.strip().lower()
+
+    redirect = fake_data.redirects.get(base_url)
+    return redirect
+
+
+def all_pages() -> List[Dict]:
+    return list(fake_data.pages.values())
+
+
+def all_redirects() -> List[Dict]:
+    return list(fake_data.redirects.values())
+
+
+def get_redirect_by_id(redirect_id: int) -> Optional[dict]:
+    for url, redirect in fake_data.redirects.items():
+        if redirect.get('id') == redirect_id:
+            return redirect
+
+    return None
+
+
+def get_page_by_id(page_id: int) -> Optional[dict]:
+    for url, page in fake_data.pages.items():
+        if page.get('id') == page_id:
+            return page
+
+    return None
+
+
+def create_redirect(name: str, short_url: str, url: str):
+    if get_redirect(short_url):
+        raise Exception("Cannot create redirect, exists!")
+
+    data = {
+        'id': random.randint(100, 1000000),
+        'url': url,
+        'short_url': short_url,
+        'name': name,
+    }
+
+    fake_data.redirects[short_url] = data
+
+
+def update_redirect(redirect_id, name, short_url, url):
+    redirect = get_redirect_by_id(redirect_id)
+
+    if not redirect:
+        raise Exception("Cannot update redirect, does not exist!")
+
+    del fake_data.redirects[redirect.get('short_url')]
+
+    redirect['short_url'] = short_url
+    redirect['url'] = url
+    redirect['name'] = name
+
+    fake_data.redirects[short_url] = redirect
+
+
+def create_page(title, url, contents):
+    if get_page(url):
+        raise Exception("Cannot create page, exists!")
+
+    data = {
+        'id': random.randint(100, 1000000),
+        'url': url,
+        'title': title,
+        'contents': contents,
+    }
+
+    fake_data.pages[url] = data
+
+
+def update_page(page_id: int, title: str, url: str, contents: str):
+    page = get_page_by_id(page_id)
+
+    if not page:
+        raise Exception("Cannot update page, does not exist!")
+
+    del fake_data.pages[page.get('url')]
+
+    page['title'] = title.strip()
+    page['url'] = url.strip().lower()
+    page['contents'] = contents.strip()
+
+    fake_data.pages[url] = page
---
+++ code/flask/ch07-saving-content/pypi_flask_ch07/pypi_org/views/admin_views.py
@@ -0,0 +1,143 @@
+import flask
+
+from pypi_org.infrastructure import permissions
+from pypi_org.infrastructure.view_modifiers import response
+from pypi_org.services import cms_service
+from pypi_org.viewmodels.admin.editpage_viewmodel import EditPageViewModel
+from pypi_org.viewmodels.admin.editredirect_viewmodel import EditRedirectViewModel
+from pypi_org.viewmodels.admin.pageslist_viewmodel import PagesListViewModel
+from pypi_org.viewmodels.admin.redirectlist_viewmodel import RedirectListViewModel
+from pypi_org.viewmodels.shared.viewmodelbase import ViewModelBase
+
+blueprint = flask.Blueprint('admin', __name__, template_folder='templates')
+
+
+@blueprint.route('/admin')
+@permissions.admin
+@response(template_file='admin/index.html')
+def index():
+    vm = ViewModelBase()
+    return vm.to_dict()
+
+
+@blueprint.route('/admin/redirects')
+@permissions.admin
+@response(template_file='admin/redirects.html')
+def redirects():
+    vm = RedirectListViewModel()
+    return vm.to_dict()
+
+
+@blueprint.route('/admin/pages')
+@permissions.admin
+@response(template_file='admin/pages.html')
+def pages():
+    vm = PagesListViewModel()
+    return vm.to_dict()
+
+
+# ADD_REDIRECT VIEWS ####################################
+#
+#
+@blueprint.route('/admin/add_redirect', methods=['GET'])
+@permissions.admin
+@response(template_file='admin/edit_redirect.html')
+def add_redirect_get():
+    vm = EditRedirectViewModel()
+    return vm.to_dict()
+
+
+@blueprint.route('/admin/add_redirect', methods=['POST'])
+@permissions.admin
+@response(template_file='admin/edit_redirect.html')
+def add_redirect_post():
+    vm = EditRedirectViewModel()
+    vm.process_form()
+
+    if not vm.validate():
+        return vm.to_dict()
+
+    cms_service.create_redirect(vm.name, vm.short_url, vm.url)
+
+    return flask.redirect('/admin/redirects')
+
+
+# EDIT_REDIRECT VIEWS ####################################
+#
+#
+@blueprint.route('/admin/edit_redirect/<int:redirect_id>', methods=['GET'])
+@permissions.admin
+@response(template_file='admin/edit_redirect.html')
+def edit_redirect_get(redirect_id: int):
+    vm = EditRedirectViewModel(redirect_id)
+    if not vm.redirect:
+        return flask.abort(404)
+
+    return vm.to_dict()
+
+
+@blueprint.route('/admin/edit_redirect/<int:redirect_id>', methods=['POST'])
+@permissions.admin
+@response(template_file='admin/edit_redirect.html')
+def edit_redirect_post(redirect_id: int):
+    vm = EditRedirectViewModel(redirect_id)
+    vm.process_form()
+
+    if not vm.validate():
+        return vm.to_dict()
+
+    cms_service.update_redirect(vm.redirect_id, vm.name, vm.short_url, vm.url)
+
+    return flask.redirect('/admin/redirects')
+
+
+# ADD_PAGE VIEWS ####################################
+#
+#
+@blueprint.route('/admin/add_page', methods=['GET'])
+@permissions.admin
+@response(template_file='admin/edit_page.html')
+def add_page_get():
+    vm = EditPageViewModel()
+    return vm.to_dict()
+
+
+@blueprint.route('/admin/add_page', methods=['POST'])
+@permissions.admin
+@response(template_file='admin/edit_page.html')
+def add_page_post():
+    vm = EditPageViewModel()
+    vm.process_form()
+
+    if not vm.validate():
+        return vm.to_dict()
+
+    cms_service.create_page(vm.title, vm.url, vm.contents)
+
+    return flask.redirect('/admin/pages')
+
+
+# EDIT_PAGE VIEWS ####################################
+#
+#
+@blueprint.route('/admin/edit_page/<int:page_id>', methods=['GET'])
+@permissions.admin
+@response(template_file='admin/edit_page.html')
+def edit_page_get(page_id):
+    vm = EditPageViewModel(page_id)
+    return vm.to_dict()
+
+
+@blueprint.route('/admin/edit_page/<int:page_id>', methods=['POST'])
+@permissions.admin
+@response(template_file='admin/edit_page.html')
+def edit_page_post(page_id: int):
+    vm = EditPageViewModel(page_id)
+    vm.process_form()
+
+    if not vm.validate():
+        return vm.to_dict()
+
+    cms_service.update_page(vm.page_id, vm.title, vm.url, vm.contents)
+
+    return flask.redirect('/admin/pages')
