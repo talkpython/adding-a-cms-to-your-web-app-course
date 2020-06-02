ef114963942958f4c2984e04be7fc1bb72b9c774
Michael Kennedy <mikeckennedy@gmail.com>
2020-06-02 11:25:44 -0700

    Now rendering markdown rather than HTML.

--- code/flask/ch08-markdown/pypi_flask_ch08/pypi_org/viewmodels/cms/request_viewmodel.py
+++ code/flask/ch08-markdown/pypi_flask_ch08/pypi_org/viewmodels/cms/request_viewmodel.py
@@ -1,4 +1,5 @@
 import flask
+import markdown2
 
 from pypi_org.services import cms_service
 from pypi_org.viewmodels.shared.viewmodelbase import ViewModelBase
@@ -12,7 +13,8 @@ class RequestViewModel(ViewModelBase):
         self.page = cms_service.get_page(self.url)
         self.html = None
         if self.page:
-            self.html = self.page.contents
+            self.html = self.convert_to_markdown(   self.page.contents)
+            # self.html = self.convert_to_markdown((self.page.contents+'\n')*20)
 
         self.redirect = cms_service.get_redirect(self.url)
         self.redirect_url = None
@@ -23,3 +25,15 @@ class RequestViewModel(ViewModelBase):
                 query = query.decode('utf-8')
                 dest = f'{dest}?{query}'
             self.redirect_url = dest
+
+    # noinspection PyMethodMayBeStatic
+    def convert_to_markdown(self, md_text) -> str:
+        options = [
+            "cuddled-lists",
+            "code-friendly",
+            "fenced-code-blocks",
+            "tables",
+        ]
+
+        html = markdown2.markdown(md_text, extras=options, safe_mode=True)
+        return html
